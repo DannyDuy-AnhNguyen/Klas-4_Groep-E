ef47709b3069badd5c30929f56543c03ac10feb8
Hamza Harsal <hamzaharsal10@gmail.com>
2025-06-16 13:07:39 +0200

    hhh

--- src/main/java/Game/item/HiddenEasterEgg.java
+++ src/main/java/Game/item/HiddenEasterEgg.java
@@ -9,7 +9,7 @@ public class HiddenEasterEgg {
                 speler.getInventory().stream().allMatch(i -> i.getNaam().equalsIgnoreCase("Rots"));
 
         if (alleRotsen) {
-            System.out.println("🥚 EASTER EGG: Je inventory zit VOL met nutteloze rotsen. 🤡");
+            System.out.println(" EASTER EGG: Je inventory zit VOL met nutteloze rotsen. ");
         }
     }
 }
